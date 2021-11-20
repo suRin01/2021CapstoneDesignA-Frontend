b8bf30560b9156faf5f1014f72111b280ccc5c4b
soya-s2 <soya.s2.dev@gmail.com>
2021-11-20 14:23:06 +0900

    로고 삽입과 크기 / 위치 조정

--- src/components/NavigationBar/NavLeft.jsx
+++ src/components/NavigationBar/NavLeft.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import logoImg from "logo.png";
 
 const Container = styled.section`
   /* 임시 */
   font-weight: bold;
   font-size: 2rem;
+
+  img {
+    width: 4rem;
+    align-items: center;
+  }
 `;
 
 const NavLeft = () => {
   return (
     <Container>
       <li>
-        <Link to="/">LOGO</Link>
+        <Link to="/">
+          <img src={logoImg} />
+        </Link>
       </li>
     </Container>
   );
