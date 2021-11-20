3858285736073f51374b05025e815a9ef65c431e
soya-s2 <soya.s2.dev@gmail.com>
2021-11-20 14:25:40 +0900

    게시글 생성 페이지 컴포넌트와 뒤로 가기 버튼, 이미지 업로드 버튼 추가
    - 이미지 업로드는 file input tag를 사용했으나
    기존의 UI를 덮어쓴 새로운 이미지 업로드 버튼을 만들어
    ref를 지정하여 이벤트가 동작하게 하였음.
    
    - 게시글을 작성하면 content가 디버깅 콘솔로 확인 가능하고,
    이미지를 업로드하면 이미지 change 이벤트에 의해
    이미지 객체를 콘솔에서 디버깅할 수 있도록 하였음.
    
    - 서버와 연동이 아직 계획되지 않아
    axios나 formData를 사용한 로직은 작성되지 않았음.

--- src/pages/WritePostPage.jsx
+++ src/pages/WritePostPage.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, useHistory } from "react-router-dom";
 import styled from "styled-components";
+import "../css/font.css";
 
 // 사용자 정의 hook
 import useTextArea from "../hooks/useTextArea";
@@ -9,25 +10,89 @@ import useTextArea from "../hooks/useTextArea";
 // import { apiAppendPost, apiEditPost, apiFetchPost } from "../api";
 
 const Wrapper = styled.form`
+  margin-top: 1rem;
   flex-grow: 1;
-
+  font-family: "Noto Sans KR", sans-serif;
   border-radius: 1rem;
+  background-color: #d4f0e7;
+  padding: 1.6rem;
+  font-weight: 600;
+  position: relative;
 
   textarea {
     width: 100%;
     padding: 1rem;
     resize: none;
     overflow: hidden;
-    box-shadow: 3px 3px 20px gray;
-    font-size: 1rem;
+    font-size: 1.4rem;
+    font-family: "Noto Sans KR", sans-serif;
+    border-radius: 1rem;
+    border: none;
     height: 300px;
+    margin-bottom: 3.1rem;
   }
+
+  button {
+    font-family: "Noto Sans KR", sans-serif;
+    position: absolute;
+    font-size: 1.2rem;
+    color: white;
+    background-color: #56a87b;
+    right: 1.3em;
+    padding: 0.4rem 1rem;
+    font-weight: 500;
+    border-radius: 1rem;
+    bottom: 1em;
+  }
+
+  /* 뒤로가기 버튼 */
+  input[type="button"]:nth-last-of-type(1) {
+    cursor: pointer;
+    font-family: "Noto Sans KR", sans-serif;
+    position: absolute;
+    font-size: 1.2rem;
+    background-color: #bccec6;
+    right: 10.5rem;
+    padding: 0.4rem 0.9rem;
+    font-weight: 500;
+    border-radius: 1rem;
+    border: none;
+    bottom: 1em;
+  }
+
+  /* 이미지 업로드 버튼 */
+  input[type="button"]:nth-last-of-type(2) {
+    cursor: pointer;
+    font-family: "Noto Sans KR", sans-serif;
+    position: absolute;
+    font-size: 1.2rem;
+    background-color: #d4cbe9;
+    left: 1.6rem;
+    padding: 0.4rem 0.9rem;
+    font-weight: 500;
+    border-radius: 1rem;
+    border: none;
+    bottom: 1em;
+  }
+
+  /* 보통의 파일 업로드 방식, 기존 UI를 논렌더링 처리하고 이미지 업로드 버튼에 이벤트를 등록함 */
+  input[type="file"] {
+    display: none;
+  }
+`;
+
+const Div = styled.div`
+  display: inline-block;
+  font-size: 1.6rem;
+  padding-bottom: 1rem;
 `;
 
 const WritePostPage = ({ history, match }) => {
   const [content, onChangeContent, setContent, resize] = useTextArea("");
   const textareaRef = useRef();
   const [PostId] = useState(match.params.PostId);
+  const userImgInput = useRef();
+  const [files, setFiles] = useState("");
 
   // 게시글 수정이라면 해당 게시글 정보 가져와서 textarea에 넣기
   useEffect(async () => {
@@ -42,9 +107,6 @@ const WritePostPage = ({ history, match }) => {
 
     if (PostId) {
       if (confirm("게시글을 수정할까요?")) {
-        // 게시글 생성 api호출
-        // apiAppendPost({ content })
-
         // 홈페이지 강제이동
         history.push("/");
       }
@@ -52,7 +114,7 @@ const WritePostPage = ({ history, match }) => {
       if (confirm("게시글을 생성할까요?")) {
         // 게시글 수정 api호출
         // apiEditPost({ PostId, content })
-
+        console.log("게시글 생성");
         // 홈페이지 강제이동
         history.push("/");
       }
@@ -69,8 +131,33 @@ const WritePostPage = ({ history, match }) => {
     [content],
   );
 
+  // '뒤로' 버튼을 눌렀을 때 첫 화면으로 돌아감
+  const goBack = useCallback(() => {
+    console.log("맨 처음 페이지로 이동");
+    history.push("/");
+  });
+
+  // 이미지 업로드 버튼을 클릭하면 기존의 이벤트를 삭제하고 ref에 지정된 사용자 정의 컴포넌트로 이벤트를 연결
+  const ImgInputBtnClick = e => {
+    event.preventDefault();
+    userImgInput.current.click();
+  };
+
+  // 이미지 업로드가 완료되면 파일을 읽어오는 디버깅 코드. files state를 update함. 즉 서버에 보낼 이미지 객체임.
+  // 현 코드에서 서버 연동을 위한 axois는 고려되지 않음.
+  const onLoadImg = e => {
+    const file = e.target.files;
+    console.log(file);
+    setFiles(file);
+  };
+
   return (
     <Wrapper onSubmit={onSubmit}>
+      <Div>게시글 생성하기</Div>
+      <input type="file" ref={userImgInput} multiple accept="image/*" onChange={onLoadImg} />
+      <input type="button" value="📷 이미지 업로드" onClick={ImgInputBtnClick} />
+      <input type="button" value="뒤로" onClick={goBack} />
+      <button type="submit">💚 게시하기</button>
       <textarea
         ref={textareaRef}
         value={content}
@@ -80,9 +167,8 @@ const WritePostPage = ({ history, match }) => {
           resize(textareaRef.current, 300);
         }}
         onKeyUp={() => resize(textareaRef.current, 300)}
-        placeholder={`어떤 생각을 하고 계신가요...?\n이미지처리는 나중에 추가하겠음`}
+        placeholder={`어떤 생각을 하고 계신가요?`}
       />
-      <button type="submit">submit</button>
     </Wrapper>
   );
 };
